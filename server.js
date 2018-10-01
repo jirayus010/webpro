bd726f39ab35abd5f7bcb132f5168b0c6062d3e3
Jirayus Phithaksan <lnwnackza.jp@gmail.com>
2018-10-01 16:14:33 +0700

    Edit port setting

--- server.js
+++ server.js
@@ -141,5 +141,7 @@ app.post('/product/update', function (req, res) {
     console.log('UPDATE:' +sql);
     res.redirect('/products');
 });
-console.log('App is running at http://localhost:8080');
-app.listen(8080);
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+console.log('App is running on http://localhost:' + port);
+});
