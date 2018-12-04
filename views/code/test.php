ad912c15a5c243814fbc8edcce917d33a2b1d1e8
supavadi <supavadi_bell@kjst.ac.th>
2018-12-04 19:23:25 +0700

    aaaaaaaaa

--- views/code/test.php
+++ views/code/test.php
@@ -3,7 +3,7 @@
 <h1><center>Owner Website</center></h1>
 <h2>Name: <%= fullname %></h2>
 <h2>Name: <%= fullname2 %></h2>
-<h2>Name: <%= fullnam3 %></h2>
+<h2>Name: <%= fullname3 %></h2>
 
 </div>
 <center><img src="http://www.pngall.com/wp-content/uploads/2016/06/Cartoon.png" width="100" hight="200"></center>
